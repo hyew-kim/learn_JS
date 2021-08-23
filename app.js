9293049ed8d12fbf87f768058d5b3b570ee29286
Kim Hyewon <gpdnjs861@naver.com>
2021-08-23 14:50:30 +0900

    firstJsCalculator

---
+++ app.js
@@ -0,0 +1,19 @@
+const calculator = {
+  plus: function (a, b) {
+    console.log(a + b);
+  },
+  minus: function (a, b) {
+    console.log(a - b);
+  },
+  multiple: function (a, b) {
+    console.log(a * b);
+  },
+  div: function (a, b) {
+    console.log(a / b);
+  },
+};
+
+calculator.plus(10, 2);
+calculator.minus(10, 2);
+calculator.multiple(10, 2);
+calculator.div(10, 2);
